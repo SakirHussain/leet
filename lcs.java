10b06ef2e047888866b066018f877d8a4e131c18
sakirhussain <sakirhussainpa@gmail.com>
2024-05-06 23:07:28 +0530

    lcs n lis

--- lcs.java
+++ lcs.java
@@ -13,11 +13,14 @@ public class lcs {
     public static int helper(int i, int j, String str1, String str2, Integer[][] dp) {
         if (i == str1.length() || j == str2.length())
             return 0;
+        
         if (dp[i][j] != null)
             return dp[i][j];
+        
         if (str1.charAt(i) == str2.charAt(j))
             return 1 + helper(i + 1, j + 1, str1, str2, dp);
-        int first = helper(i + 1, j, str1, str2, dp);
+        
+        int first = helper(i + 1, j, str1, str2, dp);   
         int second = helper(i, j + 1, str1, str2, dp);
         int answer = Math.max(first, second);
         dp[i][j] = answer;
